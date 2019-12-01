9e3cb988bba9e9a8c5dfff5c23f62ba8a7a84c6e
Aleksandar Vacić <av@radianttap.com>
2019-12-01 18:36:46 +0100

    Removed Demo project, re-added files for proper framework

---
+++ Framework/Framework.h
@@ -0,0 +1,19 @@
+//
+//  SCPageViewController.h
+//  SCPageViewController
+//
+//  Created by Aleksandar Vacić on 12/1/19.
+//  Copyright © 2019 Radiant Tap. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SCPageViewController.
+FOUNDATION_EXPORT double SCPageViewControllerVersionNumber;
+
+//! Project version string for SCPageViewController.
+FOUNDATION_EXPORT const unsigned char SCPageViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SCPageViewController/PublicHeader.h>
+
+
